a5edb00df91d59936c095a62c8348acebb1b2334
Daniel <lamdaniel@hotmail.ca>
2022-06-22 16:08:43 -0700

    testing added and completed

--- testPrac.js
+++ testPrac.js
@@ -21,4 +21,33 @@ const calculator = {
   },
 };
 
-export { capitalize, reverseString };
+//shifts 5
+function caesarCipher(str) {
+  str = str.toLowerCase().split('');
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == '' || str[i].charCodeAt() < 97 || str[i].charCodeAt() > 122) {
+      continue;
+    } else {
+      let strCharCode = str[i].charCodeAt();
+      strCharCode += 5;
+      if (strCharCode > 122) {
+        strCharCode -= 26;
+      }
+      str[i] = String.fromCharCode(strCharCode);
+    }
+  }
+  str = str.join('');
+  return str;
+}
+
+function analyzeArray(arr) {
+  const obj = {
+    average: arr.reduce((a, b) => a + b, 0) / arr.length,
+    min: Math.min(...arr),
+    max: Math.max(...arr),
+    length: arr.length,
+  };
+  return obj;
+}
+
+export { capitalize, reverseString, calculator, caesarCipher, analyzeArray };
--- testPrac.test.js
+++ testPrac.test.js
@@ -1,4 +1,10 @@
-import { capitalize, reverseString } from './testPrac.js';
+import {
+  capitalize,
+  reverseString,
+  calculator,
+  caesarCipher,
+  analyzeArray,
+} from './testPrac.js';
 
 test('hello to be Hello', () => {
   expect(capitalize('hello')).toBe('Hello');
@@ -14,4 +20,39 @@ test('cereal to be laerec', () => {
 
 test('racecar to be racecar', () => {
   expect(reverseString('racecar')).toBe('racecar');
+});
+
+test('1 + 2 = 3', () => {
+  expect(calculator.add(1, 2)).toBe(3);
+});
+test('5 + 8 = 13', () => {
+  expect(calculator.add(5, 8)).toBe(13);
+});
+test('5 - 2 = 3', () => {
+  expect(calculator.subtract(5, 2)).toBe(3);
+});
+test('5 * 3 = 15', () => {
+  expect(calculator.multiply(5, 3)).toBe(15);
+});
+test('15 / 5 = 3', () => {
+  expect(calculator.divide(15, 5)).toBe(3);
+});
+
+test('"attack at dawn" is encrypted to "fyyfhp fy ifbs"', () => {
+  expect(caesarCipher('attack at dawn')).toBe('fyyfhp fy ifbs');
+});
+
+test('"wake up tomorrow at 9:00am sharp" is encrypted to "bfpj zu ytrtwwtb fy 9:00fr xmfwu"', () => {
+  expect(caesarCipher('wake up tomorrow at 9:00am sharp')).toBe(
+    'bfpj zu ytrtwwtb fy 9:00fr xmfwu'
+  );
+});
+
+test('[1,8,3,4,2,6] to return avg: 4, min: 1, max: 8, length: 6 in an object', () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
 });
